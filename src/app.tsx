88567aaf8728f630148a9c0dd579112b1dba6d8c
James Gower <jgower.dev@gmail.com>
2021-09-16 14:41:53 +0100

    config updated for localhost/prod redirect urls

--- src/app.tsx
+++ src/app.tsx
@@ -9,9 +9,52 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import awsExports from "./aws-exports";
 import Notifier from "./utils/components/Notifier";
+import { isLocalhost, hasLocalhost, hasHostname } from "./utils";
+
+const urlsIn = awsExports.oauth.redirectSignIn.split(",");
+const urlsOut = awsExports.oauth.redirectSignOut.split(",");
+
+const oauth = {
+  domain: awsExports.oauth.domain,
+  scope: awsExports.oauth.scope,
+  redirectSignIn: awsExports.oauth.redirectSignIn,
+  redirectSignOut: awsExports.oauth.redirectSignOut,
+  responseType: awsExports.oauth.responseType,
+};
+
+/**
+ * Sets the correct oauth redirect sign-in/sign-out variables depending on
+ * if the user is accessing from the localhost or production.
+ */
+if (isLocalhost) {
+  urlsIn.forEach((e: string): void => {
+    if (hasLocalhost(e)) {
+      oauth.redirectSignIn = e; // will be localhost
+    }
+  });
+  urlsOut.forEach((e: string): void => {
+    if (hasLocalhost(e)) {
+      oauth.redirectSignOut = e; // will be localhost
+    }
+  });
+} else {
+  urlsIn.forEach((e: string): void => {
+    if (hasHostname(e)) {
+      oauth.redirectSignIn = e; // will be xxx.amplifyapp.com
+    }
+  });
+  urlsOut.forEach((e: string): void => {
+    if (hasHostname(e)) {
+      oauth.redirectSignOut = e; // will be xxx.amplifyapp.com
+    }
+  });
+}
+
+const configUpdate = awsExports;
+configUpdate.oauth = oauth;
 
 Amplify.configure({
-  ...awsExports,
+  ...configUpdate,
   Auth: {
     mandatorySignIn: false,
   },
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -37,4 +37,17 @@ export const getPostData = async (id: string): Promise<Post> => {
   return data?.getPost as Post;
 };
 
+// amplify
+// helper methods for setting up aws_exports with localhost and production simultaneously
+export const hasLocalhost = (hostname: string): boolean =>
+  Boolean(
+    hostname.match(/localhost/) ||
+      hostname.match(/127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/),
+  );
+
+export const hasHostname = (hostname: string): boolean =>
+  Boolean(hostname.includes(window.location.hostname));
+
+export const isLocalhost = hasLocalhost(window.location.hostname);
+
 export const breakpoints = createBreakpoints({});
